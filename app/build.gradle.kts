edf85a1c202f6eefdc0596aa554f706121999060
Erbolbro <144798174+Erbolbro@users.noreply.github.com>
2024-05-11 11:58:40 +0600

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,102 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    // Kapt
+    kotlin("kapt")
+
+    // Hilt
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.example.teamworklesson7compose"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.teamworklesson7compose"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Hilt
+    //noinspection UseTomlInstead
+    implementation("com.google.dagger:hilt-android:2.48")
+    //noinspection UseTomlInstead
+    kapt("com.google.dagger:hilt-android-compiler:2.48")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    //noinspection UseTomlInstead
+    implementation("org.jetbrains:annotations:RELEASE_VERSION")
+
+    // Retrofit_2
+    //noinspection UseTomlInstead
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    //noinspection UseTomlInstead
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.6")
+
+    // OkHttp
+    //noinspection UseTomlInstead
+    implementation(platform("com.squareup.okhttp3:okhttp-bom:4.12.0"))
+    //noinspection UseTomlInstead
+    implementation("com.squareup.okhttp3:okhttp")
+    //noinspection UseTomlInstead
+    implementation("com.squareup.okhttp3:logging-interceptor")
+
+    //AsyncImage
+    implementation("io.coil-kt:coil-compose:2.6.0")
+}
